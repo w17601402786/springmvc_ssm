826be08f0ad450712d6362419e8c134c1de4d649
w17601402786 <72337244+w17601402786@users.noreply.github.com>
2023-05-08 18:33:33 +0800

    又给赵英杰找了一堆事情

---
+++ src/test/java/com/management/service/ClassInfoServiceTest.java
@@ -0,0 +1,18 @@
+package com.management.service;
+
+import junit.framework.TestCase;
+
+public class ClassInfoServiceTest extends TestCase {
+
+    public void testAddClassInfo() {
+    }
+
+    public void testUpdateClassInfo() {
+    }
+
+    public void testDeleteClassInfoById() {
+    }
+
+    public void testGetClassInfo() {
+    }
+}
