cff195d91c4f5374eb4c98fae528d34484234d51
w17601402786 <72337244+w17601402786@users.noreply.github.com>
2023-05-08 10:08:40 +0800

    增加Service权限验证

--- src/main/java/com/management/service/UsersService.java
+++ src/main/java/com/management/service/UsersService.java
@@ -5,23 +5,23 @@ import java.util.List;
 
 public interface UsersService {
     // 添加用户
-    int addUser(Users user);
+    int addUser(Users user, String thisUserType);
 
     // 根据用户名查找用户
-    Users getUserByUsername(String username);
+    Users getUserByUsername(String username, String thisUserType);
 
     // 更新用户信息
-    int updateUser(Users user);
+    int updateUser(Users user,String thisUserType);
 
     // 根据用户ID删除用户
-    int deleteUserById(Integer userId);
+    int deleteUserById(Integer userId, String thisUserType);
 
     // 获取所有用户
-    List<Users> getAllUsers();
+    List<Users> getAllUsers(String thisUserType);
 
     // 获取特定类型的所有用户
-    List<Users> getUsersByType(String userType);
+    List<Users> getUsersByType(String userType, String thisUsertype);
 
     // 根据用户ID获取用户信息
-    Users getUserById(Integer userId);
+    Users getUserById(Integer userId, String thisUserType);
 }
--- src/main/java/com/management/service/impl/UsersServiceImpl.java
+++ src/main/java/com/management/service/impl/UsersServiceImpl.java
@@ -24,38 +24,70 @@ public class UsersServiceImpl implements UsersService {
     private TeacherMapper teacherMapper;
 
     @Override
-    public int addUser(Users user) {
+    public int addUser(Users user, String thisUserType) {
+
+        if (!thisUserType.equals("admin")){
+            return -1;
+        }
+
         return usersMapper.addUser(user);
     }
 
     @Override
-    public Users getUserByUsername(String username) {
+    public Users getUserByUsername(String username,String thisUserType) {
+
+        if (!thisUserType.equals("admin")){
+            return null;
+        }
+
         return usersMapper.getUserByUsername(username);
     }
 
     @Override
-    public int updateUser(Users user) {
+    public int updateUser(Users user,String thisUserType) {
+
+        if (!thisUserType.equals("admin")){
+            return -1;
+        }
+
         return usersMapper.updateUser(user);
     }
 
     @Override
-    public int deleteUserById(Integer userId) {
+    public int deleteUserById(Integer userId,String thisUserType) {
+        if (!thisUserType.equals("admin")){
+            return -1;
+        }
         return 0;
     }
 
     @Override
-    public List<Users> getAllUsers() {
+    public List<Users> getAllUsers(String thisUserType) {
+
+        if (!thisUserType.equals("admin")){
+            return null;
+        }
+
         return usersMapper.getAllUsers();
     }
 
     @Override
-    public List<Users> getUsersByType(String userType) {
+    public List<Users> getUsersByType(String userType,String thisUsertype) {
+
+        if (!thisUsertype.equals("admin")){
+            return null;
+        }
 
         return null;
     }
 
     @Override
-    public Users getUserById(Integer userId) {
+    public Users getUserById(Integer userId,String thisUserType) {
+
+        if (!thisUserType.equals("admin")){
+            return null;
+        }
+
         return null;
     }
 }
